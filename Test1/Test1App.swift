638a25677cf3de6602962c502ce5fb0b64cf9b94
margaux mazaleyras <margaux.crvx@gmail.com>
2024-11-18 14:17:32 +0100

    Initial Commit

---
+++ Test1/Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test1App.swift
+//  Test1
+//
+//  Created by Margaux Mazaleyras on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
